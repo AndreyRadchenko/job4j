1485b5921431d8d58f4d266b70402452790c57b6
AndreyRadchenko <radchenko.a.s@yandex.ru>
2019-05-16 22:04:50 +0300

    1.3. JavaDoc. Code convention[#141132]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
+/**
+ * @author Radchenko Andrey
+ * @since 16.05.2019
+ * @version 1.0
+ */
 package ru.job4j.calculate;
 
 public class Calculate {
-	public static void main (String[] args) {
+   /**
+   *Вывод строки в консоль
+   *@param args - args
+   */	
+  public static void main (String[] args) {
 		System.out.println ("Hello World");
 	}
 
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Radchenko (radchenko@yandex.ru)
+* @version $Id$
+* @since 16.05.2019
+*/
+package ru.job4j.calculate;
