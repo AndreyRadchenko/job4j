79988363516ab7442f8bd44018c679066e628795
AndreyRadchenko <radchenko.a.s@yandex.ru>
2019-05-27 15:46:43 +0300

    4.1. Глупый бот.[#141088]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+/**
+ * Программа отвечает на переданные строки
+ * @author Radchenko Andrey (radchenko@yandex.ru)
+ * @since 27.05.2019
+ * @version 1.0
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author Andrey Radchenko (radchenko@yandex.ru)
+ * @since 27.05.2019
+ * @version 1.0
+ */
+public class DummyBotTest {
+
+    /**
+     * Тест фразы "Привет, Бот."
+     */
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    /**
+     * Тест фразы "Пока."
+     */
+    @Test
+    public void whenByeBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    /**
+     * Тест любой фразы, кроме "Привет, Бот." и "Пока."
+     */
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}
