df1805095309e4237b5a36dba9b7ae084ac4e373
AndreyRadchenko <radchenko.a.s@yandex.ru>
2019-05-27 16:34:44 +0300

    4.2. Максимум из двух чисел[#141091]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+/**
+ * Программа вычисляет максимальное из двух чисел
+ * @author Radchenko Andrey (radchenko@yandex.ru)
+ * @since 27.05.2019
+ * @version 1.0
+ */
+public class Max {
+
+    /**
+     * Сравнивает числа и возвращает максимальное из них
+     * @param first Первое число
+     * @param second Второе число
+     * @return Результат
+     */
+    public int max(int first, int second) {
+
+        return first > second ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author Andrey Radchenko (radchenko@yandex.ru)
+ * @since 27.05.2019
+ * @version 1.0
+ */
+public class MaxTest {
+
+    /**
+     * Тест. Первое чило больше
+     */
+    @Test
+    public void whenMax1To2Then2() {
+        Max max = new Max();
+        int result = max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    /**
+     * Тест. Второе чило больше
+     */
+    @Test
+    public void whenMax3To1Then3() {
+        Max max = new Max();
+        int result = max.max(3, 1);
+        assertThat(result, is(3));
+    }
+
+    /**
+     * Тест. Числа одинаковые
+     */
+    @Test
+    public void when2To2Then2() {
+        Max max = new Max();
+        int result = max.max(2, 2);
+        assertThat(result, is(2));
+    }
+}
