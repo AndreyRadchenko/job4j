3950c4493e64f2ee6f33544b754b8cf4456b10e4
AndreyRadchenko <radchenko.a.s@yandex.ru>
2019-05-30 15:54:06 +0300

    5.3. Построить шахматную доску в псевдографике.[#141103]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+/**
+ * Программа для построения шахматной доски в псевдографике
+ * @author Radchenko Andrey (radchenko@yandex.ru)
+ * @since 30.05.2019
+ * @version 1.0
+ */
+public class Board {
+
+    /**
+     * Строит строковую таблицу по заданным параметрам
+     * @param width Символов по ширине
+     * @param height Символов по высоте
+     * @return Псевдографика
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if ((i + j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author Andrey Radchenko (radchenko@yandex.ru)
+ * @since 30.05.2019
+ * @version 1.0
+ */
+public class BoardTest {
+
+    /**
+     * Тест. Строим доску 3 на 3
+     */
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X%s X %sX X%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тест. Строим доску 5 на 4
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X X%s X X %sX X X%s X X %s", line, line, line, line);
+        assertThat(result, is(expected)); //напишите здесь тест, проверяющий формирование доски 5 на 4.
+    }
+}
