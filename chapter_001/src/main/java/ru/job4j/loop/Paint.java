0c41fa3b9074e87b2bc156d0475e35376e9550f8
AndreyRadchenko <radchenko.a.s@yandex.ru>
2019-06-02 19:56:56 +0300

    5.4. Построить пирамиду в псевдографике.[#141102]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -4,10 +4,59 @@ package ru.job4j.loop;
  * Программа для построения пирамиды в псевдографике
  * @author Radchenko Andrey (radchenko@yandex.ru)
  * @since 30.05.2019
- * @version 1.0
+ * @version 2.0
  */
 public class Paint {
 
+    /**
+     * Строит правую часть пирамиды
+     * @param height Высота пирамиды
+     * @return Результат
+     */
+    public String rightTrl(int height) {
+        // Буфер для результата.
+        StringBuilder screen = new StringBuilder();
+        // ширина будет равна высоте.
+        int width = height;
+        // внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+            // внутренний цикл определяет положение ячейки в строке.
+            for (int column = 0; column != width; column++) {
+                // если строка равна ячейки, то рисуем галку.
+                // в данном случае строка определяем, сколько галок будет на строке
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод строки.
+            screen.append(System.lineSeparator());
+        }
+        // Получаем результат.
+        return screen.toString();
+    }
+
+    /**
+     * Строит левую часть пирамиды
+     * @param height Высота пирамиды
+     * @return Результат
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= width - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
     /**
      * Строит пирамиду заданной высоты
      * @param  height Высота пирамиды
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -9,15 +9,46 @@ import static org.junit.Assert.*;
  * Test
  * @author Andrey Radchenko (radchenko@yandex.ru)
  * @since 30.05.2019
- * @version 1.0
+ * @version 2.0
  */
 public class PaintTest {
 
     /**
-     * Тест. Строим пирамиду 4 строки высотой
+     * Тест. Строим правую часть пирамиды 4 строки высотой
      */
     @Test
     public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("^   ")
+                .add("^^  ")
+                .add("^^^ ")
+                .add("^^^^")
+                .toString()));
+    }
+
+    /**
+     * Тест. Строим левую часть пирамиды 4 строки высотой
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("   ^")
+                .add("  ^^")
+                .add(" ^^^")
+                .add("^^^^")
+                .toString()));
+    }
+    /**
+     * Тест. Строим пирамиду 4 строки высотой
+     */
+    @Test
+    public void whenPyramid4Full() {
         Paint paint = new Paint();
         String rst = paint.pyramid(4);
         System.out.println(rst);
