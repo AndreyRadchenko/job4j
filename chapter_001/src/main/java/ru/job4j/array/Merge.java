51d6a9d0a70c4f6dbeb77593390961491b21644c
AndreyRadchenko <radchenko.a.s@yandex.ru>
2019-06-13 22:12:54 +0300

    Экзамен. Базовый синтаксис.[#141140] - Исправлена ver.2.0

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -1,29 +1,52 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 /**
- * Объединение 2х массивов и сортировка
+ * Объединение 2х массивов в порядке сортировки
  * @author Radchenko Andrey (radchenko@yandex.ru)
  * @since 11.06.2019
- * @version 1.0
+ * @version 2.0
  */
 public class Merge {
 
     /**
-     * Объединяет 2 массива и сортирует результат
+     * Объединяет 2 массива в порядке сортировки
      * @param left первый массив
      * @param right второй массив
      * @return объеденённый массив
      */
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
+        if (rsl.length == left.length) {
+            rsl = Arrays.copyOf(left, left.length);
+        } else if (rsl.length == right.length) {
+            rsl = Arrays.copyOf(right, right.length);
+        } else {
+            int j = 0;
+            int g = 0;
+            for (int i = 0; i < rsl.length; i++) {
+                if (left[j] < right[g]) {
+                    rsl[i] = left[j];
+                    if (j == left.length - 1) {
+                        while (g < right.length) {
+                            rsl[++i] = right[g++];
+                        }
+                        break;
+                    }
+                    j++;
+                } else {
+                    rsl[i] = right[g];
+                    if (g == right.length - 1) {
+                        while (j < left.length) {
+                            rsl[++i] = left[j++];
+                        }
+                        break;
+                    }
+                    g++;
+                }
+            }
         }
-        for (int i = 0; i < right.length; i++) {
-            rsl[left.length + i] = right[i];
-        }
-        BubbleSort rslt = new BubbleSort();
-        rsl = rslt.sort(rsl);
         return rsl;
     }
 }
