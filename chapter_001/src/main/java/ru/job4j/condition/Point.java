6a6ba83ced81c606bacf1fdf2cd5144afebb60bd
AndreyRadchenko <radchenko.a.s@yandex.ru>
2019-07-03 22:15:46 +0300

    Рефакторинг - Расстояние между точками.[#141216]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -7,17 +7,36 @@ import static java.lang.Math.sqrt;
  * Программа расчёта расстояния между двумя точками в плоской системе координат
  */
 public class Point {
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
 
     /**
-     * Расчёт расстояния по формуле
-     * @param x1 Координата x первой точки
-     * @param y1 Координата x первой точки
-     * @param x2 Координата x первой точки
-     * @param y2 Координата x первой точки
-     * @return Расстояние.
+     * И это поле объекта. Оно доступно только конкретному объекту.
      */
-    public double distance(int x1, int y1, int x2, int y2) {
+    private int y;
 
-        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    /**
+     *
+     * @param that обьект тип Point
+     * @return Расстояние
+     */
+    public double distance(Point that) {
+
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
